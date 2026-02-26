23e7dfca1e4a0de90901f7f53a2e2f38ea14aad2
B28LH <benlloydhurwitz@gmail.com>
2026-02-26 10:32:42 +0300

    Touch up of similarity, adding playwright

--- assets/jsxgraph/similarity-intro.js
+++ assets/jsxgraph/similarity-intro.js
@@ -1,6 +1,3 @@
-
-
-
 var board = JXG.JSXGraph.initBoard('jsxgraph-similarity-intro', {
     boundingbox: [-2, 25, 35, -5],
     axis: false,
@@ -27,7 +24,7 @@ var k = board.create('slider', [[2, 22], [15, 22], [0.5, 2, 4]], {
    2. Original Triangle ABC
 ---------------------------- */
 // FIX: Removed "fixed: true" so the student can drag the vertices
-var A = board.create('point', [0, 0], {name: 'A', color: 'blue', size: 4});
+var A = board.create('point', [0, 0], {name: 'A', color: 'blue', size: 4, label: {offset: [-15, -15]}});
 var B = board.create('point', [7, 0], {name: 'B', color: 'blue', size: 4});
 var C = board.create('point', [30/7, (6 * Math.sqrt(24)) / 7], {name: 'C', color: 'blue', size: 4});
 
@@ -44,7 +41,7 @@ var polyABC = board.create('polygon', [A, B, C], {
 var P = board.create('point', [
     function() { return A.X() + 12; },
     function() { return A.Y(); }
-], {name: 'P', color: 'red', fixed: true});
+], {name: 'P', color: 'red', fixed: true, label: {offset: [-15, -15]}});
 
 var Q = board.create('point', [
     function() { return P.X() + k.Value() * (B.X() - A.X()); },
@@ -81,53 +78,28 @@ function angleText(pt1, pt2, pt3) {
 
 /* --- Original Triangle Labels --- */
 
-board.create('text', [
-    function(){ return (A.X() + B.X())/2; }, 
-    function(){ return (A.Y() + B.Y())/2 - 1; },
-    lengthText(A,B)
-], {fontSize: 14, color: 'blue'});
+board.create('midpoint', [A, B], {name: lengthText(A,B), size: 0, label: {offset: [-20, -20]}, fontSize:14, color:'blue'}); // Invisible point to help position the label
 
-board.create('text', [
-    function(){ return (A.X() + C.X())/2 - 1; }, 
-    function(){ return (A.Y() + C.Y())/2 + 0.5; },
-    lengthText(A,C)
-], {fontSize: 14, color: 'blue'});
+board.create('midpoint', [A, C], {name: lengthText(A,C), size: 0, label: {offset: [-40, 10]}, fontSize:14, color:'blue'}); // Invisible point to help position the label
 
-board.create('text', [
-    function(){ return (B.X() + C.X())/2 + 0.5; }, 
-    function(){ return (B.Y() + C.Y())/2 + 0.5; },
-    lengthText(B,C)
-], {fontSize: 14, color: 'blue'});
+board.create('midpoint', [B, C], {name: lengthText(B,C), size: 0, label: {offset: [10, 10]}, fontSize:14, fontColor:'blue'}); // Invisible point to help position the label
 
 // Interior angles for ABC
-board.create('angle', [B, A, C], {name: angleText(B, A, C), radius: 1, fillColor: '#00FF00'});
-board.create('angle', [C, B, A], {name: angleText(C, B, A), radius: 1, fillColor: '#00FF00'});
-board.create('angle', [A, C, B], {name: angleText(A, C, B), radius: 1, fillColor: '#00FF00'});
+board.create('angle', [B, A, C], {name: angleText(B, A, C), radius: 1.3, fillColor: '#00FF00', fontSize: 10});
+board.create('angle', [C, B, A], {name: angleText(C, B, A), radius: 1.3, fillColor: '#00FF00', fontSize: 10});
+board.create('angle', [A, C, B], {name: angleText(A, C, B), radius: 1.3, fillColor: '#00FF00', fontSize: 10});
 
 /* --- Similar Triangle Labels --- */
 
-board.create('text', [
-    function(){ return (P.X() + Q.X())/2; }, 
-    function(){ return (P.Y() + Q.Y())/2 - 1; },
-    lengthText(P,Q)
-], {fontSize: 14, color: 'red'});
+board.create('midpoint', [P, Q], {name: lengthText(P,Q), size: 0, label: {offset: [-20, -20]}, fontSize:16, color:'red', anchorX:'middle'}); // Invisible point to help position the label
 
-board.create('text', [
-    function(){ return (P.X() + R.X())/2 - 1; }, 
-    function(){ return (P.Y() + R.Y())/2 + 0.5; },
-    lengthText(P,R)
-], {fontSize: 14, color: 'red'});
+board.create('midpoint', [P, R], {name: lengthText(P,R), size: 0, label: {offset: [-40, 10]}, fontSize:16, color:'red'}); // Invisible point to help position the label
 
-board.create('text', [
-    function(){ return (Q.X() + R.X())/2 + 0.5; }, 
-    function(){ return (Q.Y() + R.Y())/2 + 0.5; },
-    lengthText(Q,R)
-], {fontSize: 14, color: 'red'});
-
+board.create('midpoint', [Q, R], {name: lengthText(Q,R), size: 0, label: {offset: [10, 10]}, fontSize:16, fontColor:'red'}); // Invisible point to help position the label
 // Corresponding interior angles for PQR
-board.create('angle', [Q, P, R], {name: angleText(Q, P, R), radius: 1, fillColor: '#FFD700'});
-board.create('angle', [R, Q, P], {name: angleText(R, Q, P), radius: 1, fillColor: '#FFD700'});
-board.create('angle', [P, R, Q], {name: angleText(P, R, Q), radius: 1, fillColor: '#FFD700'});
+board.create('angle', [Q, P, R], {name: angleText(Q, P, R), radius: function() { return k.Value(); }, fillColor: '#FFD700'});
+board.create('angle', [R, Q, P], {name: angleText(R, Q, P), radius: function() { return k.Value(); }, fillColor: '#FFD700'});
+board.create('angle', [P, R, Q], {name: angleText(P, R, Q), radius: function() { return k.Value(); }, fillColor: '#FFD700'});
 
 /* ----------------------------
    5. Smooth Slider Update
