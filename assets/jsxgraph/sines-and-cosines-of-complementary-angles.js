118bbd9f1b02d46a6a639bfad8221e540d5b54f2
B28LH <benlloydhurwitz@gmail.com>
2026-02-27 14:32:10 +0300

    Tweaks to complementary angles interactive

--- assets/jsxgraph/sines-and-cosines-of-complementary-angles.js
+++ assets/jsxgraph/sines-and-cosines-of-complementary-angles.js
@@ -1,6 +1,5 @@
-
 var board = JXG.JSXGraph.initBoard('jsxgraph-complementary', {
-    boundingbox: [-2, 10, 12, -5],
+    boundingbox: [-2, 10, 12, -3.7],
     axis: false,
     grid: false,
     showNavigation: false,
@@ -8,52 +7,62 @@ var board = JXG.JSXGraph.initBoard('jsxgraph-complementary', {
 });
 
 // Angle Slider (alpha)
-var s = board.create('slider', [[1, 9], [5, 9], [10, 30, 80]], {
-    name: '&theta;',
-    suffixLabel: '&deg;',
-    snapWidth: 1
+var s = board.create('slider', [[-1, 9], [5, 9], [10, 30, 80]], {
+    suffixLabel: '&theta; = ',
+    postLabel: '&deg;',
+    snapWidth: 1,
+    label: { position: 'top', fontSize: 14, color: 'blue' }
 });
 
 // Points & Triangle
 // Fixed hypotenuse length for visual consistency
 var hypLen = 8; 
 
-var O = board.create('point', [0, 0], { name: 'O', fixed: true, color: 'black' });
-var C = board.create('point', [
+var A = board.create('point', [0, 0], { name: 'A', fixed: true, color: 'black', label: {offset:[-15,-15]}});
+var B = board.create('point', [
     function() { return hypLen * Math.cos(s.Value() * Math.PI / 180); },
     0
-], { name: 'C', visible: true, color: 'black', size: 2 });
+], { name: 'B', visible: true, color: 'black', size: 2, label: {offset: [10, -10]}});
 
-var B = board.create('point', [
-    function() { return C.X(); },
+var C = board.create('point', [
+    function() { return B.X(); },
     function() { return hypLen * Math.sin(s.Value() * Math.PI / 180); }
-], { name: 'B', color: 'red', size: 3 });
+], { name: 'C', color: 'red', size: 3 });
 
-var tri = board.create('polygon', [O, C, B], {
+var tri = board.create('polygon', [A, B, C], {
     fillColor: '#FFFF00',
     fillOpacity: 0.1,
     borders: { strokeColor: 'black', strokeWidth: 2 }
 });
 
 // Angles
-// Angle at O (Theta)
-var angleTheta = board.create('angle', [C, O, B], {
+// Angle at A (Theta)
+var angleTheta = board.create('angle', [B, A, C], {
     name: function() { return '&theta; = ' + s.Value() + '&deg;'; },
     radius: 1.2,
     fillColor: 'blue',
-    fillOpacity: 0.2
+    fillOpacity: 0.2,
+    label: { offset: [0, -5], fontSize: 14}
 });
 
+
 // Angle at B (90 - Theta)
-var angleComp = board.create('angle', [O, B, C], {
+var angleComp = board.create('angle', [A, C, B], {
     name: function() { return '&phi; = ' + (90 - s.Value()) + '&deg;'; },
     radius: 1.2,
     fillColor: 'green',
-    fillOpacity: 0.2
+    fillOpacity: 0.2,
+    label: { offset: [-5, -25], fontSize: 14}
+});
+
+var angleRight = board.create('angle', [C, B, A], {
+    radius: 0.6,
+    name: '',
+    label: {visible: false}
 });
 
 // Right angle symbol
-// board.create('ticks', [board.create('segment', [O, C]), 1], {
+// board.create('ticks', [board.create('segment', [A, C]), 1], {
 //     drawLabels: false,
 //     label: { offset: [0, 0] }
 // }); // Just a visual helper
@@ -72,23 +81,23 @@ board.create('text', [6, -1.0, function() {
 // SINE COMP
 board.create('text', [0, -2.0, function() {
     var val = Math.sin(s.Value() * Math.PI / 180).toFixed(3);
-    return "sin(&theta;) = BC / OB = <b>" + val + "</b>";
+    return "sin(&theta;) = BC / AC = <b>" + val + "</b>";
 }], { fontSize: 13 });
 
 board.create('text', [6, -2.0, function() {
     var val = Math.cos((90 - s.Value()) * Math.PI / 180).toFixed(3);
-    return "cos(&phi;) = BC / OB = <b>" + val + "</b>";
+    return "cos(&phi;) = BC / AC = <b>" + val + "</b>";
 }], { fontSize: 13 });
 
 // COSINE COMP
 board.create('text', [0, -3.0, function() {
     var val = Math.cos(s.Value() * Math.PI / 180).toFixed(3);
-    return "cos(&theta;) = OC / OB = <b>" + val + "</b>";
+    return "cos(&theta;) = AB / AC = <b>" + val + "</b>";
 }], { fontSize: 13 });
 
 board.create('text', [6, -3.0, function() {
     var val = Math.sin((90 - s.Value()) * Math.PI / 180).toFixed(3);
-    return "sin(&phi;) = OC / OB = <b>" + val + "</b>";
+    return "sin(&phi;) = AB / AC = <b>" + val + "</b>";
 }], { fontSize: 13 });
 
 // Summary Note
